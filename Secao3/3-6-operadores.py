e56b888b575f3b6436fbd001a5a53c019c04cca0
Alvaro Ito <alvaro.e.ito@gmail.com>
2024-04-29 17:05:35 -0300

    att

---
+++ Secao3/3-6-operadores.py
@@ -0,0 +1,26 @@
+adicao = 10 + 10
+print(f'Adição {adicao}')
+
+subtracao = 10 - 5
+print(f'Subtração {subtracao}')
+
+multiplicacao = 10 * 10
+print(f'Multiplicação {multiplicacao}')
+
+divisao = 10 / 3  # float
+print(f'Divisão {divisao}')
+
+divisao_inteira = 10 // 3
+print(f'Divisão inteira {divisao_inteira}')
+
+exponenciacao = 2 ** 10
+print(f'Exponenciação {exponenciacao}')
+
+modulo = 55 % 2  # resto da divisão
+print(f'Módulo {modulo}')
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
