797267ffeb5fad0c00a71caf7fc5164b41acd2d3
Alvaro Ito <alvaro.e.ito@gmail.com>
2024-05-21 17:04:31 -0300

    att

---
+++ Secao4/4-10-empacotamento-desempacotamento-dict.py
@@ -0,0 +1,27 @@
+
+# Empacotamento e desempacotamento de dicionários
+a, b = 1, 2
+a, b = b, a
+# print(a, b)
+
+
+# (a1, a2), (b1, b2) = pessoa.items()
+# print(a1, a2)
+# print(b1, b2)
+
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
+
+pessoa = {
+    'nome': 'Aline',
+    'sobrenome': 'Souza',
+}
+
+dados_pessoa = {
+    'idade': 16,
+    'altura': 1.6,
+}
+
+pessoas_completa = {**pessoa, **dados_pessoa}
+# print(pessoas_completa)
+
---
+++ Secao4/4-11-args-kwargs.py
@@ -0,0 +1,22 @@
+# args e kwargs
+# args (já vimos)
+# kwargs - keyword arguments (argumentos nomeados)
+
+
+def mostro_argumentos_nomeados(*args, **kwargs):
+    print('NÃO NOMEADOS:', args)
+
+    for chave, valor in kwargs.items():
+        print(chave, valor)
+
+
+# mostro_argumentos_nomeados(nome='Joana', qlq=123)
+# mostro_argumentos_nomeados(**pessoas_completa)
+
+configuracoes = {
+    'arg1': 1,
+    'arg2': 2,
+    'arg3': 3,
+    'arg4': 4,
+}
+mostro_argumentos_nomeados(**configuracoes)
