7e9d3c462d77ca788c0ba17a2513acbe99b75370
Alvaro Ito <alvaro.e.ito@gmail.com>
2024-10-30 15:20:48 -0300

    att

--- Secao6/aula17_site/requests.py
+++ Secao6/aula17_site/requests.py
@@ -3,11 +3,6 @@ import requests
 
 
 url = 'http://localhost:8000/'
-try:
-    response = requests.get(url)
-    response.raise_for_status()  # Lança um erro se o status não for 200
-    print(f'Resposta: {response.text}')
-except requests.exceptions.RequestException as e:
-    print(f'Erro na requisição: {e}')
-
-input("Pressione Enter para sair...")
+response = requests.get(url)
+print(response.status_code)
+print(response.text)
