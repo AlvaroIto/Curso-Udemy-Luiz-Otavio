cb9f6527232c5de815b3ba11025f8b2f4b900250
Alvaro Ito <alvaro.e.ito@gmail.com>
2024-11-06 10:22:49 -0300

    Update main.py

--- Secao6/aula18_selenium/main.py
+++ Secao6/aula18_selenium/main.py
@@ -3,8 +3,10 @@ from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Caminho para a raiz do projeto
 ROOT_FOLDER = Path(__file__).parent
@@ -26,11 +28,24 @@ def make_chrome_browser(*options: str) -> webdriver.Chrome:
     return browser
 
 if __name__ == '__main__':
+    TIME_TO_WAIT = 10
+
     # Example
     options = ('--disable-gpu', '--no-sandbox',)
     browser = make_chrome_browser(*options)
 
     #Como antes
     browser.get('https://www.google.com')
-    sleep(10)
+
+    # Espere para encontrar o input
+    search_input = WebDriverWait(browser, TIME_TO_WAIT).until(EC.presence_of_element_located((By.NAME, 'q')))
+    search_input.send_keys('Hello World!')
+    search_input.send_keys(Keys.ENTER)
+    results = browser.find_element(By.ID, 'search')
+    links = results.find_elements(By.TAG_NAME, 'a')
+    links[0].click()
+
+
+    # Dorme por 10 segundos
+    sleep(TIME_TO_WAIT)
 
